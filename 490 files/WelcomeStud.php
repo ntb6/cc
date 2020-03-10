511451c8903d4315a7d534db3a03a2ca328753eb
ntb6 <56608716+ntb6@users.noreply.github.com>
2020-03-09 22:26:05 -0400

    Create WelcomeStud.php

---
+++ 490 files/WelcomeStud.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+  <link rel="stylesheet" href="WelcomeStud3.css">
+</head>
+<body>
+  <?php include 'studnavbar.php';?>
+<div class="container">
+  <!-- Header -->
+</html>
+
+</div>
+<!-- Main Container Ends -->
+</body>
+</html>
